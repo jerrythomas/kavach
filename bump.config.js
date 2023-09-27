89d3462e94085cc19a614ad95662f3087354f365
Jerry Thomas <me@jerrythomas.name>
2023-09-27 18:46:31 +0530

    chore: added bumpp config file

---
+++ bump.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'bumpp'
+
+export default defineConfig({
+  files: [
+    'package.json',
+    'pnpm-lock.yaml',
+    'packages/*/package.json',
+    'adapters/*/package.json',
+    'shared/*/package.json',
+    'sites/*/package.json',
+  ],
+  recursive: true,
+})
