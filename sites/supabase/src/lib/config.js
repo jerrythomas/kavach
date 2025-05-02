9dd81e176284111abc1bbe930c71172842c77e4a
Jerry Thomas <me@jerrythomas.name>
2025-05-02 11:48:51 -0500

    feat: modified config to reuse supabase config

--- sites/supabase/src/lib/config.js
+++ sites/supabase/src/lib/config.js
@@ -10,8 +10,5 @@ export const appConfig = {
 		level: env.PUBLIC_LOG_LEVEL,
 		table: env.PUBLIC_LOG_TABLE
 	},
-	supabase: {
-		url: env.PUBLIC_SUPABASE_URL,
-		anonKey: env.PUBLIC_SUPABASE_ANON_KEY
-	}
+	supabase: supabaseConfig
 }
