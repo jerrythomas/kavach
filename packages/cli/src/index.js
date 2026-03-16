57a254cfd8b1cfff835cf91b7be81363612a9455
Jerry Thomas <me@jerrythomas.name>
2026-03-16 17:53:26 -0500

    feat(cli): register kavach doctor command

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 import { init } from './commands/init.js'
 import { add } from './commands/add.js'
+import { doctor } from './commands/doctor.js'
 
 const [command, ...args] = process.argv.slice(2)
 
 if (command === 'init') await init()
 else if (command === 'add') await add(args[0])
-else console.log('Usage: kavach <init|add auth-page|add routes>')
+else if (command === 'doctor') await doctor(args.includes('--fix'))
+else console.log('Usage: kavach <init|add auth-page|add routes|doctor [--fix]>')
